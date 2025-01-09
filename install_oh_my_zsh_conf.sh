bcdeaa9e7430efc58749b99576c8fda133f15967
csotelo <carlos.sotelo@viasat.com>
2025-01-09 15:06:55 -0500

    WIP

--- install_oh_my_zsh_conf.sh
+++ install_oh_my_zsh_conf.sh
@@ -2,6 +2,7 @@
 SCRIPT_DIR=$(dirname "$(realpath $0)")
 
 if [ -d "${HOME}/.oh-my-zsh/themes" ]; then
-  ln -sf ${SCRIPT_DIR}/.oh-my-zsh/themes/charly.zsh-theme ~/.oh-my-zsh/themes/charly.zsh-theme
-  sed -i 's/ZSH_THEME="[^"]*"/ZSH_THEME="charly"/' ~/.zshrc
+  echo "Installing charly.zsh-theme..."
+  ln -sf ${SCRIPT_DIR}/.oh-my-zsh/themes/charly.zsh-theme ${HOME}/.oh-my-zsh/themes/charly.zsh-theme
+  sed -i 's/ZSH_THEME="[^"]*"/ZSH_THEME="charly"/' ${HOME}/.zshrc
 fi

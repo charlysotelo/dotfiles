60157646a63ce55f59c18c93cbe90a447ba67def
csotelo <carlos.sotelo@viasat.com>
2025-01-13 11:22:02 -0500

    Update

--- install_nvim_conf.sh
+++ install_nvim_conf.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 SCRIPT_DIR=$(dirname "$(realpath $0)")
 
+# Install NvChad
 git clone https://github.com/NvChad/starter ${HOME}/.config/nvim
+
+# Install copilot
+git clone https://github.com/copilot.vim.git ${HOME}/.config/nvim/pack/github/start/copilot.vim
+

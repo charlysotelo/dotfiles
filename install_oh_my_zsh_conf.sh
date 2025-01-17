262bd5190657fc7edee4bf12a60777b3382e8cf2
csotelo <carlos.sotelo@viasat.com>
2025-01-17 00:40:37 +0000

    History settings

--- install_oh_my_zsh_conf.sh
+++ install_oh_my_zsh_conf.sh
@@ -6,3 +6,9 @@ if [ -d "${HOME}/.oh-my-zsh/themes" ]; then
   ln -sf ${SCRIPT_DIR}/.oh-my-zsh/themes/charly.zsh-theme ${HOME}/.oh-my-zsh/themes/charly.zsh-theme
   sed -i 's/ZSH_THEME="[^"]*"/ZSH_THEME="charly"/' ${HOME}/.zshrc
 fi
+
+cat << EOF > ${HOME}/.zshrc
+export HISTSIZE=10000000
+export SAVEHIST=10000000
+setopt SHARE_HISTORY
+EOF

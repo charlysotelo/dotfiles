f1a2bf3b0d1437f0c9242f43e62893d0bd0ab10d
Carlos Sotelo <charlysotelo187@gmail.com>
2025-04-10 13:54:24 +0000

    Install neovim

--- install_nvim_conf.sh
+++ install_nvim_conf.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 SCRIPT_DIR=$(dirname "$(realpath $0)")
 
+# Install neovim if it isn't installed already
+if ! command -v nvim > /dev/null 2>&1; then
+  TEMP_DIR="/tmp/nvim-install"
+  BIN_NAME="nvim-linux-$(uname -m)"
+  TARBALL_NAME="${BIN_NAME}.tar.gz"
+  TARBALL_URL="https://github.com/neovim/neovim/releases/latest/download/${TARBALL_NAME}"
+  mkdir -p "$TEMP_DIR"
+  pushd "$TEMP_DIR"
+  curl -LO "$TARBALL_URL"
+  tar xzf "${TARBALL_NAME}" --strip-components=1 -C ${HOME}/.local/
+  rm -rf $TEMP_DIR
+  sed -i '/alias vim='\''nvim'\''/!{$s/$/\nalias vim='\''nvim'\''/}' ~/.zshrc
+  popd
+fi
+
 # Install NvChad
 git clone https://github.com/NvChad/starter ${HOME}/.config/nvim
 

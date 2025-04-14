c5bf16eb572ad80d20db15a39257aa18eb03f0e1
Carlos Sotelo <charlysotelo187@gmail.com>
2025-04-14 20:48:48 +0000

    Add ATUIN

---
+++ install_atuin.sh
@@ -0,0 +1,12 @@
+# Install atuin if it isn't installed already
+if ! command -v atuin > /dev/null 2>&1; then
+  curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
+  sed -i 's/(atuin init zsh)/(atuin init zsh --disable-up-arrow)/g' ~/.zshrc
+  source ${HOME}/.atuin/bin/env
+fi
+
+if [ ${ATUIN_PERSONAL_LOGIN} = "true" ]; then
+  atuin login -u ${ATUIN_USERNAME} -p ${ATUIN_PASSWORD} -k "${ATUIN_KEY}"
+  atuin sync
+fi
+
--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ SCRIPT_DIR=$(dirname "$(realpath $0)")
 ${SCRIPT_DIR}/install_tmux_conf.sh
 ${SCRIPT_DIR}/install_oh_my_zsh_conf.sh
 ${SCRIPT_DIR}/install_nvim_conf.sh
+${SCRIPT_DIR}/install_atuin.sh
